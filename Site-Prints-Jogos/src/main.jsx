4a975e157019cab7a5e14b76255fbdf9a1d53a2d
Daniel-Macedo-dev <danielmacedosilva11@gmail.com>
2026-02-10 18:25:42 -0300

    add new css

--- Site-Prints-Jogos/src/main.jsx
+++ Site-Prints-Jogos/src/main.jsx
@@ -2,8 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter, HashRouter } from "react-router-dom";
 import App from "./App";
-import "./index.css";
-import "./styles/custom.css";
+import "./styles/main.css";
 import "./styles/bootstrap-lite.css";
 
 const Router = import.meta.env.PROD ? HashRouter : BrowserRouter;
