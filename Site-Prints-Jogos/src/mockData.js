ac22e7057ce7d5839fcc145bdd1ff16c2ff486d7
Daniel-Macedo-dev <danielmacedosilva11@gmail.com>
2026-02-11 23:18:03 -0300

    switch to HashRouter in production and refresh mock gallery data

--- Site-Prints-Jogos/src/mockData.js
+++ Site-Prints-Jogos/src/mockData.js
@@ -4,6 +4,7 @@ export const mockPrints = [
     game: "The Witcher 3",
     description: "Print de The Witcher 3",
     username: "Pedrin",
+    uploadDate: "2026-02-01T18:30:00.000Z",
     url: "https://images.steamusercontent.com/ugc/54713847181920584/DF0F10C40FC36DDCABA86074498C8B4FB6B832D1/?imw=5000&imh=5000&ima=fit&impolicy=Letterbox&imcolor=%23000000&letterbox=false",
   },
   {
@@ -11,6 +12,7 @@ export const mockPrints = [
     game: "Red Dead Redemption 2",
     description: "Print de Red Dead Redemption 2",
     username: "Digito",
+    uploadDate: "2026-02-03T21:10:00.000Z",
     url: "https://images.steamusercontent.com/ugc/2262560445629999117/875F8E6DB79938111244DC9F68370EFFB319E86C/?imw=5000&imh=5000&ima=fit&impolicy=Letterbox&imcolor=%23000000&letterbox=false",
   },
   {
@@ -18,6 +20,7 @@ export const mockPrints = [
     game: "Resident Evil 4",
     description: "Print de Resident Evil 4",
     username: "Thylenda",
+    uploadDate: "2026-02-06T14:45:00.000Z",
     url: "https://images.steamusercontent.com/ugc/2072275113013523325/8F166CB403F626A5337BA27306F338761E5DCE69/?imw=5000&imh=5000&ima=fit&impolicy=Letterbox&imcolor=%23000000&letterbox=false",
   },
   {
@@ -25,6 +28,7 @@ export const mockPrints = [
     game: "The Witcher 3",
     description: "Print de The Witcher 3",
     username: "LemosU",
+    uploadDate: "2026-02-09T03:20:00.000Z",
     url: "https://images.steamusercontent.com/ugc/2002450932056130936/A182724A1713DA1ED7ED247725AD1F95A614C9B3/?imw=5000&imh=5000&ima=fit&impolicy=Letterbox&imcolor=%23000000&letterbox=false",
   },
 ];
